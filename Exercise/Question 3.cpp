5a5c4eac3a3180fba47cd830a7813a08c726ca7d
Lau Yan Kai <148327378+lauyankai@users.noreply.github.com>
2024-01-13 21:23:20 +0800

    Add files via upload

---
+++ Exercise/Question 3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+/*
+			LAU YAN KAI
+			A23CS0098
+			EXERCISE 2 
+			SECJ1013 PROGRAMMING TECHNIQUE I 
+*/
+
+int main() {
+	int num, product;
+	product = 1;
+	cout << "Enter an integer number: ";
+	cin >> num;
+
+	while (num != 0) {
+		int digit;
+		digit = num % 10;
+		product *= digit;
+		cout << digit;
+		num /= 10;
+		if (num != 0) {
+			cout << " * ";
+		}
+	}
+
+	cout << " = " << product << endl; //product of the digits
+	
+	bool Multiple4 = (product % 4 == 0);
+	bool Multiple5 = (product % 5 == 0);
+	bool Multiple7 = (product % 7 == 0);
+
+	if (Multiple4)
+		cout << product << " is the multiple of 4. \n";
+	if (Multiple5) 
+		cout << product << " is the multiple of 5. \n";
+	if (Multiple7) 
+		cout << product << " is the multiple of 7.";
+	return 0;
+}
